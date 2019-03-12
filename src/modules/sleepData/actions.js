e9e5b51588911f92db06d3a1d7c7d0f95fa58c8c
sourabhdadapure <sourabhdadapure@gmail.com>
2019-03-12 14:28:39 -0400

    Adds utility

--- src/modules/sleepData/actions.js
+++ src/modules/sleepData/actions.js
@@ -1,4 +1,5 @@
 import types from './types'
+import constants from '../../utility/constants'
 import axios from 'axios'
 
 export function getSleepData() {
@@ -8,15 +9,9 @@ export function getSleepData() {
     })
 
     try {
-      let user1 = await axios.get(
-        'https://s3.amazonaws.com/eight-public/challenge/2228b530e055401f81ba37b51ff6f81d.json'
-      )
-      let user2 = await axios.get(
-        'https://s3.amazonaws.com/eight-public/challenge/d6c1355e38194139b8d0c870baf86365.json'
-      )
-      let user3 = await axios.get(
-        'https://s3.amazonaws.com/eight-public/challenge/f9bf229fd19e4c799e8c19a962d73449.json'
-      )
+      let user1 = await axios.get(constants.USER1_BASE_URL)
+      let user2 = await axios.get(constants.USER2_BASE_URL)
+      let user3 = await axios.get(constants.USER3_BASE_URL)
 
       dispatch({
         type: types.GET_SLEEP_DATA_SUCCESS,
---
+++ src/utility/constants.js
@@ -0,0 +1,8 @@
+export default {
+  USER1_BASE_URL:
+    'https://s3.amazonaws.com/eight-public/challenge/2228b530e055401f81ba37b51ff6f81d.json',
+  USER2_BASE_URL:
+    'https://s3.amazonaws.com/eight-public/challenge/d6c1355e38194139b8d0c870baf86365.json',
+  USER3_BASE_URL:
+    'https://s3.amazonaws.com/eight-public/challenge/f9bf229fd19e4c799e8c19a962d73449.json'
+}
